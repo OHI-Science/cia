70e36a688c9145e985719b5aad2d8b26632450f4
Melsteroni <frazier@nceas.ucsb.edu>
2014-12-09 10:36:21 -0800

    Updates to make the TWAP figures in wgs crs.

--- TWAP figures.R
+++ TWAP figures.R
@@ -24,23 +24,33 @@ myFiles = file.path(dir_neptune_data, "git-annex/Global/NCEAS-Pressures-Summarie
 
 
 # land/ocean layers for plots ----
-rgn_ocn_cntry <- readOGR("/var/data/ohi/model/GL-NCEAS-OceanRegions_v2013a/data", layer="rgn_ocean_cntry_mol")
+# rgn_ocn_cntry <- readOGR("/var/data/ohi/model/GL-NCEAS-OceanRegions_v2013a/data", layer="rgn_ocean_cntry_mol")
+rgn_ocn_cntry <- readOGR("/var/data/ohi/model/GL-NCEAS-OceanRegions_v2013a/data", layer="rgn_ocean_cntry_gcs")
 ocean  <-  rgn_ocn_cntry[is.na(rgn_ocn_cntry@data$ISO_3digit) & rgn_ocn_cntry@data$rgn_id==0,]
 land  <-  rgn_ocn_cntry[!is.na(rgn_ocn_cntry@data$ISO_3digit) & rgn_ocn_cntry@data$rgn_id==0,]
 rgnOHI <-  rgn_ocn_cntry[rgn_ocn_cntry@data$rgn_id>0,]
 
+plot(ocean, col="blue")
+
+## need wgs84 format
 
 # LME plots ----
 data <- read.csv('ZonalExtractionData/oneYearNorm_2013_lme.csv') 
 
 
 # LME regions for plots
-lme <- readOGR(dsn="/var/data/ohi/git-annex/Global/NCEAS-Pressures-Summaries_frazier2013/RegionMaps/newLME_Mollweide",
-                layer="newLME_Mollweide") 
+# lme <- readOGR(dsn="/var/data/ohi/git-annex/Global/NCEAS-Pressures-Summaries_frazier2013/RegionMaps/newLME_Mollweide",
+#                 layer="newLME_Mollweide") 
+
+lme <- readOGR(dsn="/var/data/ohi/git-annex/Global/NCEAS-Pressures-Summaries_frazier2013/TWAP LME summary/InputData/newLME_wgs",
+                layer="newLME") 
 
 lme@data <- join(lme@data, data, by=c("LME_NAME", "LME_NUMBER"))
 
-warm <- readOGR(dsn=file.path(myFiles, "RegionMaps/WARM"), layer="WARM_mol")
+# warm <- readOGR(dsn=file.path(myFiles, "RegionMaps/WARM"), layer="WARM_mol")
+warm <- readOGR(dsn=file.path(myFiles, "RegionMaps/WARM"), layer="WARM")
+
+
 data_warm <- read.csv('ZonalExtractionData/oneYearNorm_2013_warm.csv') %>%
   mutate(ProvCode= "WARM") %>%
   mutate(global_cumul_impact_2013_all_layers=round(global_cumul_impact_2013_all_layers, 3))
@@ -50,7 +60,7 @@ col.brks=c(0, 2.95, 3.50, 3.86, 4.31, 10)
 colors <- c('#92CDDC70', '#92D05070', '#FFFF0070', '#FFC00070', '#FF000070')
 
 # function 
-plotCHI <- function(fig=file.path(path_save, "CHI_LME.png"), shapeFile=lme, var=lme@data$global_cumul_impact_2013_all_layers){
+plotCHI <- function(fig=file.path(path_save, "CHI_LME_wgs.png"), shapeFile=lme, var=lme@data$global_cumul_impact_2013_all_layers){
   png(file=fig, width=1200, height=800, res=150, pointsize=18, type='cairo')
  par(oma=c(0,0,0,0),
       mar=c(1.5,1,0,1))
@@ -69,17 +79,18 @@ plotCHI <- function(fig=file.path(path_save, "CHI_LME.png"), shapeFile=lme, var=
   
   # plot legend
   yh=0.05; x1=p[1]+px*0.2; x2=p[2]-px*0.2; y1=p[3]; y2=p[3]+py*yh
-  ix = seq(x1, x2, length.out=10)
+  ix = seq(x1, x2, length.out=5)
   ixd = diff(ix)[1]/2
   par(xpd=TRUE) # turn off clipping to plot region
   image(x = ix,
         y = c(y1, y2),
-        z = matrix(1:10), col=colors, add=T)
+        z = matrix(1:5), col=colors, add=T)
   rect(x1-ixd,y1,x2+ixd,y2, border='gray20')
-  text(x = c(seq(x1-ixd+3247212, x2+ixd-+3247212, length.out=5)),
-       y = y1, cex=1, pos=1, # adj=c(0.5,0), # , offset=0.1,
-       labels=c(1:5)) 
-  par(xpd=F) # turn back on clipping to plot region
+  text(x = ix,
+      y = y1, cex=1, pos=1, # adj=c(0.5,0), # , offset=0.1,
+      labels=c(1:5)) 
+ 
+ par(xpd=F) # turn back on clipping to plot region
   dev.off()
 }
 plotCHI()
@@ -102,8 +113,14 @@ data_warm$cats = cut(data_warm$global_cumul_impact_2013_all_layers, col.brks, la
 fao_data <- read.csv('ZonalExtractionData/oneYearNorm_2013_fao.csv')
 
 # LME regions for plots
+# fao <- readOGR(dsn="/var/data/ohi/git-annex/Global/NCEAS-Pressures-Summaries_frazier2013/RegionMaps/FAO_regions",
+#                layer="FAO_rgns_mol")
+
+set_ll_warn(FALSE)
+set_ll_TOL(0.1)
+
 fao <- readOGR(dsn="/var/data/ohi/git-annex/Global/NCEAS-Pressures-Summaries_frazier2013/RegionMaps/FAO_regions",
-               layer="FAO_rgns_mol")
+               layer="FAO_rgns")
 fao@data <- join(fao@data, fao_data, by=c("rgn_id", "rgn_nam"))
 
 col.brks=0:5
@@ -111,7 +128,7 @@ colors <- c('#00CED170', '#92D05070', '#FFFF0070', '#FFC00070', '#FF000070')
 #1E90FF70 #cadetblue
 #104E8B70
 # function 
-plotCHI_fao <- function(fig=file.path(path_save, "CHI_FAO.png"), shapeFile=fao, var=fao@data$global_cumul_impact_2013_all_layers){
+plotCHI_fao <- function(fig=file.path(path_save, "CHI_FAO_wgs.png"), shapeFile=fao, var=fao@data$global_cumul_impact_2013_all_layers){
   png(file=fig, width=1200, height=800, res=150, pointsize=18, type='cairo')
   par(oma=c(0,0,0,0),
       mar=c(1.5,1,0,1))
@@ -128,19 +145,20 @@ plotCHI_fao <- function(fig=file.path(path_save, "CHI_FAO.png"), shapeFile=fao, 
   
   # plot legend
   yh=0.05; x1=p[1]+px*0.2; x2=p[2]-px*0.2; y1=p[3]; y2=p[3]+py*yh
-  ix = seq(x1, x2, length.out=10)
+  ix = seq(x1, x2, length.out=5)
   ixd = diff(ix)[1]/2
   par(xpd=TRUE) # turn off clipping to plot region
   image(x = ix,
         y = c(y1, y2),
-        z = matrix(1:10), col=colors, add=T)
+        z = matrix(1:5), col=colors, add=T)
   rect(x1-ixd,y1,x2+ixd,y2, border='gray20')
-  text(x = c(seq(x1-ixd, x2+ixd, length.out=6)),
+  text(x = c(seq(x1-ixd, x2+ixd, length.out=5)),
        y = y1, cex=1, pos=1, # adj=c(0.5,0), # , offset=0.1,
-       labels=c(0:5)) 
+       labels=c(1:5)) 
   par(xpd=F) # turn back on clipping to plot region
   dev.off()
 }
 
 plotCHI_fao()
  
+
